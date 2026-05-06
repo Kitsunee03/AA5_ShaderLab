2addaf497cf2557b37eb61d5f923e9f1c04a0125
PolPlasher <pollleida13@gmail.com>
2026-05-06 15:31:35 +0200

    [P3] Some improvements

--- Enti-Practica-2.2/Assets/P3 - Scanner/WorldPosFromDepth.hlsl
+++ Enti-Practica-2.2/Assets/P3 - Scanner/WorldPosFromDepth.hlsl
@@ -3,9 +3,18 @@
 
 void WorldPosFromDepth_float(float2 UV, out float3 WorldPos)
 {
+    // Samplea la profundidad cruda de la cámara
     float rawDepth = SHADERGRAPH_SAMPLE_SCENE_DEPTH(UV);
-    float4 ndc = float4(UV.x * 2.0 - 1.0, UV.y * 2.0 - 1.0,
-                        rawDepth * 2.0 - 1.0, 1.0);
+
+    // Construye coordenadas NDC (-1 a 1)
+    float4 ndc = float4(
+        UV.x * 2.0 - 1.0,
+        UV.y * 2.0 - 1.0,
+        rawDepth * 2.0 - 1.0,
+        1.0
+    );
+
+    // Transforma de clip space a world space
     float4 worldH = mul(UNITY_MATRIX_I_VP, ndc);
     WorldPos = worldH.xyz / worldH.w;
 }
